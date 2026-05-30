8708e43aae9ffb9f5d9e5d2b9516c53af4eee908
ASHONG JULIAN AKA <103359605+julian-2@users.noreply.github.com>
2026-05-30 21:33:17 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+#File: AJA.py
+#Description: A python code to print my initials using only a main function, STRING and print function.
+#Assignment Number: 2
+#
+#Name: ASHONG JULIAN AKA
+#STUDENT ID: 2425404998
+#Email: 2425404998@live.gctu.edu.gh
+#Grader: Augustus
+#
+#On my honor,ASHONG JULIAN AKA,this programming assignment is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    #This is the main function
+    print("")
+    print("...AJA")
+    print("")
+    print("........AA.............JJJJJJJJJJJJ.............AA..........")
+    print("......AA..AA...........JJJJJJJJJJJJ...........AA..AA........")
+    print(".....AAA..AAA...............JJJ..............AAA..AAA.......")
+    print("....AA......AA..............JJJ.............AA......AA......")
+    print("....AAAAAAAAAA..............JJJ.............AAAAAAAAAA......")
+    print("....AA......AA..............JJJ.............AA......AA......")
+    print("....AA......AA.........JJJ..JJJ.............AA......AA......")
+    print("....AA......AA.........JJJ..JJJ.............AA......AA......")
+    print("....AA......AA....**...JJJJJJJJ.......**....AA......AA....**")
+    print("....AA......AA....**...JJJJJJJJ.......**....AA......AA....**")
+    print("")
 
 
 main()
